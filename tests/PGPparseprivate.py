5c64eba404bc6062f3f05e0a5d4d70752e85813d
Wm Salt Hale <salt@altsalt.net>
2021-08-05 03:15:30 -0700

    added support for rootless subkeys

--- tests/PGPparseprivate.py
+++ tests/PGPparseprivate.py
@@ -4,12 +4,18 @@ import sys
 import pgpy
 from Crypto.Util.number import long_to_bytes
 
-privkey_passphrase = "G2SaK_v[ST_hS,-z"
-privkey_ascii_armor = """\
+#rootkey_passphrase = "example rootkey passphrase"
+rootkey_passphrase = "G2SaK_v[ST_hS,-z"
+subkey_passphrase = "example subkey passphrase"
+
+# import from file
+(rootkey, _) = pgpy.PGPKey.from_file('./testkey.asc')
+
+# import directly
+rootkey_ascii_armor = """\
 -----BEGIN PGP PRIVATE KEY BLOCK-----
 Version: OpenPGP.js v4.10.4
 Comment: https://openpgpjs.org
-
 xYYEXwMeqRYJKwYBBAHaRw8BAQdAbnRYP53m7rXE1huNToWCUbxD22TigANr
 0JmMV1oxr6L+CQMIpCs/vDznSZdgW38T03R+34px4fgLxw/tPMWxua+PWGlo
 QlAdUZ/+1Kg4PQg05UBNieOAWcL+Dk98ajznFIigPaqLtpQAbwlZ4AaKUD/e
@@ -27,28 +33,46 @@ tZtm/3pFBQ==
 -----END PGP PRIVATE KEY BLOCK-----
 """
 
-(privkey, _) = pgpy.PGPKey.from_blob(privkey_ascii_armor)
+(rootkey, _) = pgpy.PGPKey.from_blob(rootkey_ascii_armor)
 
-assert privkey.is_protected
-assert privkey.is_unlocked is False
+assert rootkey.is_protected
+assert rootkey.is_unlocked is False
 
 try:
-    with privkey.unlock(privkey_passphrase):
-        # privkey is now unlocked
-        assert privkey.is_unlocked
-        print('privkey is now unlocked')
-        primary_key = long_to_bytes(privkey._key.keymaterial.s)
-        print('privkey key value:')
+    with rootkey.unlock(rootkey_passphrase):
+        # rootkey is now unlocked
+        assert rootkey.is_unlocked
+        print('rootkey is now unlocked')
+        primary_key = long_to_bytes(rootkey._key.keymaterial.s)
+        print('rootkey value:')
         print("".join(["%02x" % c for c in primary_key]))
-        print('subkey key values:')
-        for key, value in privkey._children.items():
-            print('subkey id', key)
+        print('subkey values:')
+        for subkey, value in rootkey._children.items():
+            print('subkey id', subkey)
             sub_key = long_to_bytes(value._key.keymaterial.s)
-            print('subkey key value')
+            print('subkey value')
             print("".join(["%02x" % c for c in sub_key]))
         
-except: 
-    print('Unlocking failed')
+except:
+    print('Unlocking root key failed, attempting rootless subkey unlock.')
+    try:
+        print('subkey key values:')
+        for subkey, value in rootkey._children.items():
+            assert value.is_protected
+            assert value.is_unlocked is False
+            with value.unlock(subkey_passphrase):
+                # subkey is now unlocked
+                assert value.is_unlocked
+                print('subkey is now unlocked')
+                print('subkey id', subkey)
+                sub_key = long_to_bytes(value._key.keymaterial.s)
+                print('subkey value')
+                print("".join(["%02x" % c for c in sub_key]))
+            # subkey is no longer unlocked
+            assert value.is_unlocked is False
+            
+    except:
+        print('Unlocking failed')
 
-# privkey is no longer unlocked
-assert privkey.is_unlocked is False
+# rootkey is no longer unlocked
+assert rootkey.is_unlocked is False
